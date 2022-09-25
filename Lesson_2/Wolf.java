86d723eac06fe233eb45a7f133ac0f3d8e919358
darlyncarmenate <darlyncarmenate@gmail.com>
2022-09-25 21:26:54 +0200

    Wolf and WolfTeas modified

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,72 @@
 public class Wolf {
-    String name;
-    String sex;
-    float weight;
-    int age;
-    String color;
+    private String name;
+    private String sex;
+    private float weight;
+    private int age;
+    private String color;
 
-    void howl() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Incorrect age");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public void howl() {
         System.out.println("Howls");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Sits");
     }
 
-    void go() {
+    public void go() {
         System.out.println("Goes");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Runs");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Hunts");
     }
 }
