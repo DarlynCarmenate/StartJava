b5e2e6f7cd5326ad2977c3a88292ab2d5ab3f1ed
darlyncarmenate <darlyncarmenate@gmail.com>
2022-10-12 22:25:14 +0200

    Correction 2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class GuessNumber {
     private Player player1;
     private Player player2;
+    private Player currentPlayer;
 
     public GuessNumber(Player member1, Player member2) {
         player1 = member1;
@@ -14,31 +15,25 @@ public class GuessNumber {
         Random rnd = new Random();
         Scanner scn = new Scanner(System.in);
         int targetNum = rnd.nextInt(100) + 1;
-        int myNum = 0;
-        String flag = "player1";
-        String currentPlayer = player1.getName();
+        int num = 0;
+        currentPlayer = player1;
 
-        while (targetNum != myNum) {
-            System.out.println(currentPlayer + ", input a number");
-            myNum = scn.nextInt();
-            player1.setNumber(myNum);
-            currentPlayer = player2.getName();
-            if (targetNum < myNum) {
-                System.out.println("The number " + myNum + " is greater than the target number");
-            } else if (targetNum > myNum) {
-                System.out.println("The number " + myNum + " is less than the target number");
+        while (targetNum != num) {
+            
+            System.out.println(currentPlayer.getName() + ", input a number");
+            num = scn.nextInt();
+            currentPlayer.setNumber(num);
+            if (targetNum < currentPlayer.getNumber()) {
+                System.out.println("The number " + num + " is greater than the target number");
+            } else if (targetNum > currentPlayer.getNumber()) {
+                System.out.println("The number " + num + " is less than the target number");
             }
-
-            System.out.println(currentPlayer + ", input a number");
-            myNum = scn.nextInt();
-            player2.setNumber(myNum);
-            currentPlayer = player1.getName();
-            if (targetNum < myNum) {
-                System.out.println("The number " + myNum + " is greater than the target number");
-            } else if (targetNum > myNum) {
-                System.out.println("The number " + myNum + " is less than the target number");
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
+            } else {
+                currentPlayer = player1;
             }
         }
-        System.out.println("You guessed right! I picked number " + myNum);
+        System.out.println("You guessed right! I picked number " + num);
     }
 }
