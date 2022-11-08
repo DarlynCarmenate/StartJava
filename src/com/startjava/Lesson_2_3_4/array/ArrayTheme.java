09b16595f8f637a642398af8611e87bd955b7182
darlyncarmenate <darlyncarmenate@gmail.com>
2022-11-08 21:58:48 +0200

    ArrayTheme changed, 3

--- src/com/startjava/Lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/Lesson_2_3_4/array/ArrayTheme.java
@@ -4,42 +4,36 @@ import java.util.Random;
 
 public class ArrayTheme {
     public static void main(String[] args) {
+
         System.out.println("1. Реверс значений массива");
         int[] nums1 = {3, 6, 4, 1, 7, 2, 5};
         int len = nums1.length;
 
-        for (int num : nums1) {
-            System.out.print(num + " ");
-        }
+        printArray(nums1);
         System.out.println();
 
-        for (int i = 0; i < len;) {
+        for (int i = 0; i < len; i++) {
             int temp = nums1[i];
-            nums1[i++] = nums1[--len];
+            nums1[i] = nums1[--len];
             nums1[len] = temp;
         }
 
-        for (int num : nums1) {
-            System.out.print(num + " ");
-        }
+        printArray(nums1);
 
         System.out.println("\n\n2. Вывод произведения элементов массива");
         len = 10;
         int[] nums2 = new int[len];
-        int result = 1;
+        int prodDigits = 1;
 
         for (int i = 0; i < len; i++) {
             nums2[i] = i;
         }
 
-        for (int i = 0; i < len; i++) {
-            System.out.print( (nums2[i] > 0 && nums2[i] < 8) ? nums2[i] + " * " : "");
-            System.out.print( (nums2[i] == 8) ? nums2[i] + " = " : "");
-            if (nums2[i] > 0 && nums2[i] < 9) {
-                result *= nums2[i];
-            }
+        for (int i = 1; i < len - 1; i++) {
+            System.out.print((nums2[i] == 8) ? nums2[i] + " = " : nums2[i] + " * ");
+            prodDigits *= nums2[i];
         }
-            System.out.println(result);
+        System.out.println(prodDigits);
         System.out.printf("Number %d has an index %d%n", nums2[0], 0);
         System.out.printf("Number %d has an index %d", nums2[len - 1], len - 1);
 
@@ -50,9 +44,9 @@ public class ArrayTheme {
 
         for (int i = 0; i < len; i++) {
             nums3[i] = rnd.nextFloat();
-            System.out.printf("%8.3f", nums3[i]);
-            if (i == len / 2) System.out.println();
         }
+
+        printFloatArray(nums3);
 
         float midNum = nums3[len / 2 + 1];
         int count = 0;
@@ -63,56 +57,70 @@ public class ArrayTheme {
                 nums3[i] = 0;
                 count++;
             }
-            System.out.printf("%8.3f", nums3[i]);
-            if (i == len / 2) System.out.println();
         }
+
+        printFloatArray(nums3);
+
         System.out.println("\nThe number of zeroes is " + count);
 
         System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
         len = 26;
-        char[] letters = new char[len];
+        char[] alphabet = new char[len];
         for (int i = 0; i < len; i++) {
-            letters[i] = (char) ('A' + i);
+            alphabet[i] = (char) ('A' + i);
         }
 
         for (int i = 0; i < len; i++) {
             for (int j = len - 1; j >= len - 1 - i; j--) {
-                System.out.print(letters[j]);
+                System.out.print(alphabet[j]);
             }
             System.out.println();
         }
 
         System.out.println("\n5. Генерация уникальных чисел");
-        int i, j;
         len = 30;
-        int[] nums4 = new int[len];
-        int tryNum = 0;
-        for (i = 0; i < len;) {
-            tryNum = (int) (Math.random() * 40 + 60);
+        int[] uniqueNums = new int[len];
+        int trialNum = 0;
+        for (int i = 0; i < len; ) {
+            trialNum = (int) (Math.random() * 40 + 60);
+            int j;
             for (j = 0; j < i; j++) {
-                if (tryNum == nums4[j]) {
+                if (trialNum == uniqueNums[j]) {
                     break;
                 }
             }
             if (i == j) {
-                nums4[i] = tryNum;
-                System.out.print(nums4[i] + " ");
+                uniqueNums[i] = trialNum;
+                System.out.print(uniqueNums[i] + " ");
                 i++;
             }
         }
         int temp = 0;
         System.out.println();
-        for (int k = len - 1; k >= 0; k--) {
-            for (int l = 0; l < k; l++) {
-                if (nums4[l] < nums4[l + 1]) {
-                    temp = nums4[l];
-                    nums4[l] = nums4[l + 1];
-                    nums4[l + 1] = temp;
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (uniqueNums[j] < uniqueNums[j + 1]) {
+                    temp = uniqueNums[j];
+                    uniqueNums[j] = uniqueNums[j + 1];
+                    uniqueNums[j + 1] = temp;
                 }
             }
-            System.out.print(nums4[k] + " ");
+            System.out.print(uniqueNums[i] + " ");
+            if (i % 10 == 0) System.out.println();
+        }
+    }
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void printFloatArray(float[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%8.3f", arr[i]);
+            if (i == arr.length / 2) System.out.println();
         }
     }
 }
+
 
 
