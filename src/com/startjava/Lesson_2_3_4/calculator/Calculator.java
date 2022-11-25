137fbb0eb9385fff616e4838f1d2327ccc96ad25
darlyncarmenate <darlyncarmenate@gmail.com>
2022-11-25 20:28:54 +0200

    Exceptions corrected

--- src/com/startjava/Lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/Lesson_2_3_4/calculator/Calculator.java
@@ -1,28 +1,18 @@
 package com.startjava.Lesson_2_3_4.calculator;
 
 public class Calculator {
-    public static class NegativeValueException extends RuntimeException {
-
-    }
-    public static int calculate(String expression) throws NegativeValueException {
+    public static int calculate(String expression) {
         String[] splittedExpr = expression.split(" ");
-        int a = 0;
-        int b = 0;
-        try {
-            a = Integer.parseInt(splittedExpr[0]);
-            b = Integer.parseInt(splittedExpr[2]);
-        } catch (NumberFormatException e) {
-            System.out.println("Wrong data. Please, enter an integer expression ");
-            return 0;
-        }
+        int a, b;
+        a = Integer.parseInt(splittedExpr[0]);
+        b = Integer.parseInt(splittedExpr[2]);
         if (a < 0 || b < 0) {
-            throw new NegativeValueException();
-            //throw new RuntimeException("YYYY");
+            throw new IllegalArgumentException();
         }
 
         String sign = splittedExpr[1];
 
-        int result = switch (sign) {
+        return switch (sign) {
             case "+" -> a + b;
             case "-" -> a - b;
             case "*" -> a * b;
@@ -34,6 +24,5 @@ public class Calculator {
                 yield 0;
             }
         };
-        return result;
     }
 }
--- src/com/startjava/Lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/Lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,21 +2,21 @@ package com.startjava.Lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
-import static com.startjava.Lesson_2_3_4.calculator.Calculator.calculate;
-
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String answer = "yes";
         do {
             if ("yes".equals(answer)) {
-                System.out.print("Input a mathematical expression: ");
                 while (true) {
+                    System.out.print("Input a mathematical expression: ");
                     String expression = scanner.nextLine();
                     try {
-                        System.out.println(calculate(expression));
+                        System.out.println(Calculator.calculate(expression));
                         break;
-                    } catch (Calculator.NegativeValueException e) {
+                    } catch (NumberFormatException e) {
+                        System.out.println("Wrong data. Please, enter an integer expression ");
+                    } catch (IllegalArgumentException e) {
                         System.out.println("Numbers can't be negative. Input correct data");
                     }
                 }
